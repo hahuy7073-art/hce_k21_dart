e55b1e1b64c53f57b7911934ca64cc56b15af80b
hahuy7073-art <hahuy7073@gmail.com>
2026-09-12 01:48:50 +0000

    đồng bộ 12/9

---
+++ hello_dart/bin/hello_dart.dart
@@ -0,0 +1,9 @@
+import 'package:hello_dart/hello_dart.dart' as hello_dart;
+
+void main(List<String> arguments) {
+ final ten = 'Sinh vien';
+ print('Xin chao, $ten! Day la chuong trinh Dart dau tien.');
+ for (var i = 1; i <= 5; i++) {
+ print('Dem: $i');
+ }
+}
