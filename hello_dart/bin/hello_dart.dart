bf64ebbbf5bf2665915298630739f33939617112
hahuy7073-art <hahuy7073@gmail.com>
2026-09-12 02:55:04 +0000

    Lab 1: Hello Dart

--- hello_dart/bin/hello_dart.dart
+++ hello_dart/bin/hello_dart.dart
@@ -1,7 +1,7 @@
 import 'package:hello_dart/hello_dart.dart' as hello_dart;
 
 void main(List<String> arguments) {
- final ten = 'Sinh vien';
+ final ten = 'Ha Nguyen Truong Huy';
  print('Xin chao, $ten! Day la chuong trinh Dart dau tien.');
  for (var i = 1; i <= 5; i++) {
  print('Dem: $i');
